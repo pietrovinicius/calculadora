67c3e4fb959a316efa9393ebb8ee408327073f2d
Pietro Lima <pietrovinicius@hotmail.com>
2021-06-05 22:12:08 -0300

    Initial commit

---
+++ src/main/java/controle/Calculadora.java
@@ -0,0 +1,25 @@
+package controle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/Calculadora")
+public class Calculadora extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter saida = response.getWriter();
+		
+		saida.println("Meu Primeiro Servlet");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
